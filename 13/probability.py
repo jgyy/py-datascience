8693d9ac22e9be6f41a481c5aaa25d68b22e559b
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2021-09-25 15:04:28 +0800

    Probability in Finance

---
+++ 13/probability.py
@@ -0,0 +1,17 @@
+"""
+Probability in Finance
+"""
+
+
+def finance():
+    """
+    wrapper function
+    """
+    ep2 = int(0.4 * 250 + 0.6 * 0)
+    print(f"E(P2) = ${ep2}")
+    ep3 = int(0.2 * 1000 + 0.8 * 0)
+    print(f"E(P3) = ${ep3}")
+
+
+if __name__ == "__main__":
+    finance()
