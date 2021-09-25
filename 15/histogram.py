d82e4b37c34e0e80f67e7528e36f7f9022113724
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2021-09-25 22:04:52 +0800

    Histogram Exercise

--- 15/histogram.py
+++ 15/histogram.py
@@ -1,28 +1,30 @@
 """
-Numerical Variables
+Histogram Exercise
 """
 from os.path import dirname, realpath
 from numpy import arange
 from pandas import DataFrame, read_excel
+from matplotlib.pyplot import figure, show
 
 PATH = dirname(realpath(__file__))
 
 
-def numerical():
+def histogram():
     """
     wrapper function
     """
     data = DataFrame(
         read_excel(
-            io=f"{PATH}/numerical_variables.xlsx",
-            sheet_name="Frequency distribution table",
+            io=f"{PATH}/histogram_exercise.xlsx",
+            sheet_name="The histogram",
         )
     )
-    dataset = data.iloc[16:36, 1]
-    desire_inv = 6
+    dataset = data.iloc[9:29, 1]
+    print(dataset)
+    desire_inv = 10
     inv_width = (max(dataset) - min(dataset)) / desire_inv
 
-    col = [data.iloc[20, 8 + i] for i in range(4)]
+    col = [data.iloc[13, 3 + i] for i in range(4)]
     start = arange(min(dataset), max(dataset), inv_width)
     end = list(start[1:]) + [max(dataset)]
     absolute = dataset.value_counts(
@@ -32,6 +34,10 @@ def numerical():
     table = DataFrame({col[0]: start, col[1]: end, col[2]: absolute, col[3]: relative})
     print(table)
 
+    fig1 = figure().add_axes([0.1, 0.1, 0.85, 0.85])
+    fig1.hist(dataset)
+
 
 if __name__ == "__main__":
-    numerical()
+    histogram()
+    show()
