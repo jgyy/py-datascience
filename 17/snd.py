65d182f9bc51806e9660172b5091fa8eb0085511
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2021-09-26 22:57:33 +0800

    The Standard Normal Distribution Exercise

---
+++ 17/snd.py
@@ -0,0 +1,60 @@
+"""
+The Standard Normal Distribution Exercise
+"""
+from os.path import dirname, realpath
+from pandas import DataFrame, read_excel
+from matplotlib.pyplot import figure, show, hist, xlabel, ylabel, grid
+
+PATH = dirname(realpath(__file__))
+
+
+def standard():
+    """
+    wrapper function
+    """
+    data = DataFrame(
+        read_excel(
+            io=f"{PATH}/standard_normal_distribution_exercise.xlsx",
+            sheet_name="Standard normal",
+        )
+    )
+    dataset = DataFrame(data.iloc[9:999, 1].values, columns=[data.iloc[8, 1]]).dropna(
+        how="all"
+    )
+    figure("Original dataset")
+    hist(dataset)
+    xlabel("Data")
+    ylabel("Frequency")
+    grid(True)
+    odmean = round(dataset.mean(), 2).values[0]
+    odstd = round(dataset.std(), 2).values[0]
+    print(f"Original dataset: mean={odmean}, std={odstd}")
+
+    subtract = DataFrame(dataset.applymap(lambda x: x - odmean)).rename(
+        columns={"Original dataset": "Subtract mean"}
+    )
+    figure("Subtract mean")
+    hist(subtract)
+    xlabel("Data")
+    ylabel("Frequency")
+    grid(True)
+    submean = round(subtract.mean(), 2).values[0]
+    substd = round(subtract.std(), 2).values[0]
+    print(f"Subtract mean: mean={submean}, std={substd}")
+
+    standard = DataFrame(subtract.applymap(lambda x: x/substd)).rename(
+        columns={"Subtract mean": "Standardized"}
+    )
+    figure("Standardized")
+    hist(standard)
+    xlabel("Data")
+    ylabel("Frequency")
+    grid(True)
+    standmean = round(standard.mean(), 2).values[0]
+    standstd = round(standard.std(), 2).values[0]
+    print(f"Standardized: mean={standmean}, std={standstd}")
+
+
+if __name__ == "__main__":
+    standard()
+    show()
